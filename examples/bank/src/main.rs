836c0ae61099955432594492011ed462987c24ee
Ari Seyhun <ariseyhun@live.com.au>
2021-11-17 23:47:57 +0700

    chore(example/bank): remove unused code

--- examples/bank/src/main.rs
+++ examples/bank/src/main.rs
@@ -51,54 +51,5 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         .run()
         .await?;
 
-    // Create kafka config and admin client
-    // let mut consumer_config = ClientConfig::new();
-    // consumer_config
-    //     .set("group.id", env!("CARGO_PKG_NAME"))
-    //     .set("bootstrap.servers", cfg.redpanda_host.clone())
-    //     .set("allow.auto.create.topics", "true")
-    //     .set("enable.auto.commit", "true")
-    //     .set("api.version.request", "true")
-    //     .set("broker.version.fallback", "2.4.0")
-    //     .set_log_level(RDKafkaLogLevel::Debug);
-    // let mut producer_config = ClientConfig::new();
-    // producer_config
-    //     .set("bootstrap.servers", cfg.redpanda_host)
-    //     .set("api.version.request", "true")
-    //     .set("broker.version.fallback", "2.4.0")
-    //     .set_log_level(RDKafkaLogLevel::Debug);
-
-    // Create event store
-    // let event_store = PgEventStore::new_from_stringlike(&cfg.database_url, NoTls).await?;
-
-    // Create workers unordered
-    // let mut workers = FuturesUnordered::new();
-
-    // Spawn command handler
-    // workers.push(
-    //     command_handler(
-    //         event_store.clone(),
-    //         consumer_config.create().expect("Consumer creation failed"),
-    //         producer_config.create().expect("Consumer creation failed"),
-    //     )
-    //     .boxed(),
-    // );
-
-    // Spawn event handler
-    // let repository = BankAccountViewRepository::connect(&cfg.database_url, NoTls)
-    //     .await
-    //     .expect("could not connect to bank account view repository");
-    // workers.push(
-    //     event_handler(
-    //         event_store,
-    //         repository,
-    //         consumer_config.create().expect("Consumer creation failed"),
-    //     )
-    //     .boxed(),
-    // );
-
-    // Consume workers
-    // while workers.next().await.is_some() {}
-
     Ok(())
 }
