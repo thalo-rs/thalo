02e2c4fb92ceaa7ceec300bb18a018ee051ef9cd
Ari Seyhun <ariseyhun@live.com.au>
2021-11-19 00:12:51 +0700

    feat(examples/bank): use awto-es docker image

--- examples/bank/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE events (
-  id BIGSERIAL PRIMARY KEY,
-  aggregate_type TEXT NOT NULL,
-  aggregate_id TEXT NOT NULL,
-  sequence BIGINT NOT NULL,
-  event_type TEXT NOT NULL,
-  event_data JSONB NOT NULL,
-  UNIQUE (aggregate_type, aggregate_id, sequence)
-);
