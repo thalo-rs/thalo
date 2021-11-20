bd729fb08ea58cd556c83e3cb98931347507111a
Ari Seyhun <ariseyhun@live.com.au>
2021-11-21 03:10:41 +0700

    feat(awto-es-macros): add derive `MultiStreamTopic`

--- awto-es-macros/src/derive_macros/mod.rs
+++ awto-es-macros/src/derive_macros/mod.rs
@@ -4,6 +4,7 @@ pub use command::Command;
 pub use command_message::CommandMessage;
 pub use event::Event;
 pub use identity::Identity;
+pub use multi_stream_topic::MultiStreamTopic;
 pub use pg_repository::PgRepository;
 pub use stream_topic::StreamTopic;
 
@@ -13,5 +14,6 @@ mod command;
 mod command_message;
 mod event;
 mod identity;
+mod multi_stream_topic;
 mod pg_repository;
 mod stream_topic;
