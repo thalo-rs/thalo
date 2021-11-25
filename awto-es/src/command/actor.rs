7395bcb5953c18fb3a7562e5ab133446d5837d41
Ari Seyhun <ariseyhun@live.com.au>
2021-11-25 13:01:21 +0700

    feat(awto-es): add `AggregateActorPool`

--- awto-es/src/command/actor.rs
+++ awto-es/src/command/actor.rs
@@ -1,4 +1,11 @@
-use actix::{Actor, ActorFutureExt, Context, Handler, Message, ResponseActFuture, WrapFuture};
+use std::{marker::PhantomData, sync::Arc};
+
+use actix::{
+    Actor, ActorFutureExt, Addr, ArbiterHandle, Context, Handler, Message, ResponseActFuture,
+    System, WrapFuture,
+};
+use lru::LruCache;
+use tokio::sync::Mutex;
 
 use crate::{
     message::StreamTopic, Aggregate, AggregateCommandHandler, Error, Event, EventEnvelope,
@@ -16,6 +23,7 @@ where
     fn new(id: String, event_store: ES) -> Self;
 }
 
+#[derive(Clone)]
 pub struct BaseAggregateActor<ES: EventStore, A: Aggregate> {
     id: String,
     event_store: ES,
@@ -95,5 +103,90 @@ where
                 Ok(events)
             }),
         )
+    }
+}
+
+#[derive(Clone)]
+pub struct AggregateActorPool<Act, ES, A>
+where
+    Act: AggregateActor<ES, A>,
+    ES: EventStore,
+    A: Aggregate,
+    <A as Aggregate>::Command:
+        Message<Result = Result<Vec<EventEnvelope<<A as Aggregate>::Event>>, Error>>,
+{
+    actors_cache: Arc<Mutex<LruCache<String, Addr<Act>>>>,
+    aggregate: PhantomData<A>,
+    arbiter: Option<ArbiterHandle>,
+    event_store: ES,
+}
+
+impl<Act, ES, A> AggregateActorPool<Act, ES, A>
+where
+    Act: AggregateActor<ES, A>,
+    ES: EventStore + Clone + Unpin + Send + 'static,
+    A: Aggregate + Clone + Unpin + 'static,
+    <A as Aggregate>::Command: Message<Result = Result<Vec<EventEnvelope<<A as Aggregate>::Event>>, Error>>
+        + Clone
+        + Unpin
+        + 'static,
+    <A as Aggregate>::Event: Event<Aggregate = A> + StreamTopic + Unpin + 'static,
+{
+    pub fn new(event_store: ES, cache_cap: usize) -> Self {
+        let actors_cache: Arc<Mutex<LruCache<String, Addr<Act>>>> =
+            Arc::new(Mutex::new(LruCache::new(cache_cap)));
+
+        AggregateActorPool {
+            actors_cache,
+            aggregate: PhantomData,
+            arbiter: None,
+            event_store,
+        }
+    }
+
+    pub fn new_in_arbiter(arbiter: ArbiterHandle, event_store: ES, cache_cap: usize) -> Self {
+        let actors_cache: Arc<Mutex<LruCache<String, Addr<Act>>>> =
+            Arc::new(Mutex::new(LruCache::new(cache_cap)));
+
+        AggregateActorPool {
+            actors_cache,
+            aggregate: PhantomData,
+            arbiter: Some(arbiter),
+            event_store,
+        }
+    }
+
+    pub async fn handle(
+        &self,
+        id: &str,
+        command: <A as Aggregate>::Command,
+    ) -> Result<Vec<EventEnvelope<<A as Aggregate>::Event>>, Error> {
+        {
+            let mut guard = self.actors_cache.lock().await;
+            let actor = match guard.get(id) {
+                Some(actor) => actor,
+                None => {
+                    let current_system = System::try_current();
+                    let arbiter = match (&self.arbiter, &current_system) {
+                        (Some(arbiter), _) => arbiter,
+                        (None, Some(system)) => system.arbiter(),
+                        (None, None) => {
+                            return Err(Error::MissingActixSystem);
+                        }
+                    };
+
+                    let id_string = id.to_string();
+                    let event_store = self.event_store.clone();
+                    let addr =
+                        Act::start_in_arbiter(arbiter, move |_| Act::new(id_string, event_store));
+
+                    guard.put(id.to_string(), addr);
+                    guard.get(id).expect("item should exist")
+                }
+            };
+
+            actor.send(command)
+        }
+        .await?
     }
 }
