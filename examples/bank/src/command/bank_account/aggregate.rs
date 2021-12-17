7f83823413d9393799eed0387a595da6ddfd15a4
Ari Seyhun <ariseyhun@live.com.au>
2021-12-17 18:17:14 +0700

    feat(examples/bank): add invairant codes

--- examples/bank/src/command/bank_account/aggregate.rs
+++ examples/bank/src/command/bank_account/aggregate.rs
@@ -13,7 +13,10 @@ impl BankAccount {
     /// Creates a command for opening an account
     pub fn open_account(&self, initial_balance: f64) -> Result<AccountOpenedEvent, Error> {
         if self.opened {
-            return Err(Error::invariant("account already opened"));
+            return Err(Error::invariant(
+                "ACCOUNT_ALREADY_OPENED",
+                Some("account already opened"),
+            ));
         }
 
         // Reference the event created by the BankAccount::account_opened method
@@ -23,7 +26,10 @@ impl BankAccount {
     /// Deposit funds
     pub fn deposit_funds(&self, amount: f64) -> Result<FundsDepositedEvent, Error> {
         if !self.opened {
-            return Err(Error::invariant("account does not exist"));
+            return Err(Error::invariant(
+                "ACCOUNT_DOES_NOT_EXIST",
+                Some("account does not exist"),
+            ));
         }
 
         Ok(FundsDepositedEvent { amount })
@@ -32,12 +38,18 @@ impl BankAccount {
     /// Withdraw funds
     pub fn withdraw_funds(&self, amount: f64) -> Result<FundsWithdrawnEvent, Error> {
         if !self.opened {
-            return Err(Error::invariant("account does not exist"));
+            return Err(Error::invariant(
+                "ACCOUNT_DOES_NOT_EXIST",
+                Some("account does not exist"),
+            ));
         }
 
         let new_balance = self.balance - amount;
         if new_balance < 0.0 {
-            return Err(Error::invariant("insufficient funds for withdrawal"));
+            return Err(Error::invariant(
+                "INSUFFICIENT_FUNDS",
+                Some("insufficient funds for withdrawal"),
+            ));
         }
 
         Ok(FundsWithdrawnEvent { amount })
