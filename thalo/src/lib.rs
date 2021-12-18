6f784208be661b439b7660d88a4692beeb3702b4
Ari Seyhun <ariseyhun@live.com.au>
2021-12-19 00:35:13 +0700

    docs(thalo): add information example to documentation

--- thalo/src/lib.rs
+++ thalo/src/lib.rs
@@ -1,12 +1,18 @@
-//! # Thalo
+//! A framework for building large-scale event sourced systems with CQRS.
 //!
-//! Thalo is a framework for building large-scale event sourced applications with CQRS.
+//! Thalo is a framework for creating applications with an event driven approach following
+//! concepts including [**event sourcing**](https://microservices.io/patterns/data/event-sourcing.html), [**CQRS**](https://microservices.io/patterns/data/cqrs.html), [**transactional outbox**](https://microservices.io/patterns/data/transactional-outbox.html), [**event driven**](https://martinfowler.com/articles/201701-event-driven.html), [**DDD**](https://martinfowler.com/bliki/DomainDrivenDesign.html).
 //!
-//! It is being built to simplify the process of building event driven applications
-//! by simpifying the API with macros and providing the backbone, allowing developers
-//! to focus on their business requirements.
+//! It is build around some core features including:
+//! - Event Store
+//! - Aggregates
+//! - Commands & Events
+//! - Projections & View Models
+//! - Actors
 //!
-//! The goal is to make this powerful approach of developing much more accessible to developers.
+//! The goal of the framrwork is to simplify the process of building event driven applications
+//! by providing an opinionated approach through macros and providing the backbone,
+//! allowing developers to focus on their business requirements.
 //!
 //! ## Current status
 //!
@@ -17,12 +23,74 @@
 //!
 //! ## Get in touch
 //!
-//! If you'd like to ask/discuss or learn more, you can reach out via Discord.
-//! <https://github.com/tqwewe> <https://discord.gg/4Cq8NnPYPA>
+//! If you'd like to ask/discuss or learn more, you can reach via the liks below:
+//!
+//! - Github: <https://github.com/tqwewe>
+//! - Discord: <https://discord.gg/4Cq8NnPYPA>
 //!
 //! ## Example
 //!
-//! An example can be seen at <https://github.com/thalo-rs/thalo/tree/main/examples/bank>.
+//! The following is an example of a bank, showing how to define a single command and event.
+//!
+//! ```
+//! use thalo::{commands, events, Aggregate, AggregateType, Error};
+//!
+//! #[derive(Aggregate, Clone, Debug, Default)]
+//! pub struct BankAccount {
+//!     #[identity]
+//!     account_number: String,
+//!     balance: f64,
+//!     opened: bool,
+//! }
+//!
+//! #[commands]
+//! impl BankAccount {
+//!     /// Creates a command for opening an account
+//!     pub fn open_account(&self, initial_balance: f64) -> Result<AccountOpenedEvent, Error> {
+//!         if self.opened {
+//!             return Err(Error::invariant_code("ACCOUNT_ALREADY_OPENED"));
+//!         }
+//!
+//!         // Reference the event created by the BankAccount::account_opened method
+//!         Ok(AccountOpenedEvent { initial_balance })
+//!     }
+//! }
+//!
+//! #[events]
+//! impl BankAccount {
+//!     /// Creates an event for when a user opened an account
+//!     pub fn account_opened(&mut self, initial_balance: f64) {
+//!         self.balance = initial_balance;
+//!         self.opened = true;
+//!     }
+//! }
+//! ```
+//!
+//! The `#[commands]` and `#[events]` attribute macros generate a lot based on your implementations including:
+//!
+//! - `BankAccountCommand` enum
+//! - `BankAccountEvent` enum
+//! - `AccountOpenedEvent` struct
+//!
+//! _Below are the enums & structs generated:_
+//!
+//! ```
+//! pub enum BankAccountCommand {
+//!     OpenAccount {
+//!         initial_balance: f64,
+//!     },
+//! }
+//!
+//! pub enum BankAccountEvent {
+//!     AccountOpened(AccountOpenedEvent),
+//! }
+//!
+//! pub struct AccountOpenedEvent {
+//!     initial_balance: f64,
+//! }
+//! ```
+//!
+//! A full example can be seen at <https://github.com/thalo-rs/thalo/tree/main/examples/bank>.
 
 pub use actix::Message;
 pub use app::*;
