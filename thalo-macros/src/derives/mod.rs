7b265d8184f8c8f34ed4c4a207a6cfb42d5aa260
Ari Seyhun <ariseyhun@live.com.au>
2021-12-27 02:14:17 +0700

    feat(thalo-macros): add derive `Aggregate`

--- thalo-macros/src/derives/mod.rs
+++ thalo-macros/src/derives/mod.rs
@@ -1,2 +1,5 @@
+pub(crate) use aggregate::Aggregate;
 pub(crate) use typeid::TypeId;
+
+mod aggregate;
 mod typeid;
--- thalo-macros/src/lib.rs
+++ thalo-macros/src/lib.rs
@@ -8,6 +8,105 @@ mod derives;
 mod helpers;
 mod traits;
 
+/// Implements [`Aggregate`](thalo::aggregate::Aggregate) for a given struct.
+///
+/// The implementation of [`Aggregate::new`](thalo::aggregate::Aggregate::new) requires the struct to implement [`Default`].
+///
+/// # Container Attributes
+///
+/// - `#[thalo(event = "EventEnum")`
+///
+///   Specify the event enum used for [`Aggregate::Event`](thalo::aggregate::Aggregate::Event).
+///
+///   Defaults to struct name + `Event`. Eg: `struct BankAccount` would default to `BankAccountEvent`.
+///
+/// - `#[thalo(apply = "path")]`
+///
+///   Specify the path to the apply function.
+///
+///   Apply functions should have the signature:
+///     ```
+///     fn apply(aggregate: &mut Aggregate, event: AggregateEvent)
+///     ```
+///
+///   Where `Aggregate` is the struct for your aggregate, and `AggregateEvent` is the [`Aggregate::Event`](thalo::aggregate::Aggregate::Event).
+///
+///   Defaults to `"handle"`.
+///
+/// # Field Attributes
+///
+/// - `#[thalo(id)]`
+///
+///   Specify which field is the [`Aggregate::ID`](thalo::aggregate::Aggregate::ID).
+///
+///   If this attribute is not present, it defaults to the first field that is named `id`,
+///   otherwise a compile error will occur.
+///
+/// # Examples
+///
+/// Example without any attributes. All attributes are detected due to:
+///
+/// - Event enum is named `BankAccountEvent`
+/// - Aggregate has a field named `id`
+/// - There is an `apply` function in the current scope
+///
+/// ```
+/// #[derive(Default, Aggregate, TypeId)]
+/// struct BankAccount {
+///     id: String,
+///     balance: f64,
+/// }
+///
+/// fn apply(bank_account: &mut BankAccount, event: BankAccountEvent) {
+///     use BankAccountEvent::*;
+///
+///     match event {
+///         OpenedAccount { balance } => {
+///             bank_account.balance = balance;
+///         }
+///         DepositedFunds { amount } => {
+///             bank_account.balance += amount;
+///         }
+///         WithdrewFunds { amount } => {
+///             bank_account.balance -= amount;
+///         }
+///     }
+/// }
+/// ```
+///
+///  Example with attributes.
+///
+/// ```
+/// #[derive(Default, Aggregate, TypeId)]
+/// #[thalo(event = "BankEvent", apply = "apply_event")]
+/// struct BankAccount {
+///     #[thalo(id)]
+///     account_id: String,
+///     balance: f64,
+/// }
+///
+/// fn apply_event(bank_account: &mut BankAccount, event: BankEvent) {
+///     use BankEvent::*;
+///
+///     match event {
+///         OpenedAccount { balance } => {
+///             bank_account.balance = balance;
+///         }
+///         DepositedFunds { amount } => {
+///             bank_account.balance += amount;
+///         }
+///         WithdrewFunds { amount } => {
+///             bank_account.balance -= amount;
+///         }
+///     }
+/// }
+/// ```
+#[proc_macro_derive(Aggregate, attributes(thalo))]
+#[allow(non_snake_case)]
+pub fn aggregate(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    declare_derive_macro::<derives::Aggregate>(input)
+}
+
 /// Implements [`TypeId`](thalo::aggregate::TypeId) based on the struct/enum name as [snake_case](heck::ToSnakeCase).
 ///
 /// # Container Attributes
