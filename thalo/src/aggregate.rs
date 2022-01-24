be8d83d83e24b0a1ba32770b1cb83c821a0a0426
Ari Seyhun <ariseyhun@live.com.au>
2022-01-24 19:01:53 +0700

    feat(thalo): add `AggregateError`

--- thalo/src/aggregate.rs
+++ thalo/src/aggregate.rs
@@ -32,3 +32,45 @@ pub trait TypeId {
     /// Returns a unique identifier for the given type.
     fn type_id() -> &'static str;
 }
+
+/// Generic aggregate error.
+pub struct AggregateError {
+    code: Option<String>,
+    msg: Option<String>,
+}
+
+impl AggregateError {
+    /// Create a new error with a code and message.
+    pub fn new(code: impl Into<String>, msg: impl Into<String>) -> Self {
+        AggregateError {
+            code: Some(code.into()),
+            msg: Some(msg.into()),
+        }
+    }
+
+    /// Create a new error with a code only.
+    pub fn new_code(code: impl Into<String>) -> Self {
+        AggregateError {
+            code: Some(code.into()),
+            msg: None,
+        }
+    }
+
+    /// Create a new error with a message only.
+    pub fn new_message(msg: impl Into<String>) -> Self {
+        AggregateError {
+            code: None,
+            msg: Some(msg.into()),
+        }
+    }
+
+    /// Get the error code.
+    pub fn code(&self) -> Option<&String> {
+        self.code.as_ref()
+    }
+
+    /// Get the error message.
+    pub fn msg(&self) -> Option<&String> {
+        self.msg.as_ref()
+    }
+}
