4db9a1e57f91803dab34030b24defaca3bc154c5
Ari Seyhun <ariseyhun@live.com.au>
2022-01-28 22:13:59 +0700

    chore(examples/bank-account): upgrade to esdl 0.3

--- examples/bank-account/build.rs
+++ examples/bank-account/build.rs
@@ -1,5 +1,7 @@
+use esdl::codegen::{rust::RustCompiler, Compiler};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    esdl::configure()
+    Compiler::new(RustCompiler)
         .add_schema_file("./bank-account.esdl")?
         .compile()?;
 
