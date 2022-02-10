ac7be96d98e2aaa5fe55c517e01e6a7461854eee
Mike Shearer <shearerbeard@gmail.com>
2022-02-09 23:00:11 -0700

    WIP: simplify trait implementations

--- thalo-eventstoredb/src/event_store.rs
+++ thalo-eventstoredb/src/event_store.rs
@@ -17,7 +17,7 @@ use uuid::Uuid;
 
 use crate::Error;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ESDBEventPayload {
     created_at: DateTime<Utc>,
     aggregate_type: String,
@@ -51,6 +51,23 @@ impl ESDBEventStore {
     pub fn new(client: Client) -> Self {
         ESDBEventStore { client }
     }
+
+    fn stream_id<A>(
+        &self,
+        id: Option<&<A as Aggregate>::ID>
+    ) -> String
+    where
+        A: Aggregate
+    {
+        let mut stream = <A as TypeId>::type_id().to_owned();
+
+        if let Some(id_str) = id {
+            stream.push_str(&String::from("-"));
+            stream.push_str(&id_str.to_string());
+        }
+
+        stream
+    }
 }
 
 #[async_trait]
@@ -65,33 +82,25 @@ impl EventStore for ESDBEventStore {
         A: Aggregate,
         <A as Aggregate>::Event: DeserializeOwned,
     {
-        let mut stream = <A as TypeId>::type_id().to_owned();
-
-        if let Some(id_str) = id {
-            stream.push_str(&String::from(":"));
-            stream.push_str(&id_str.to_string());
-        }
+        let mut result = self
+            .client
+            .read_stream(self.stream_id::<A>(id), &Default::default(), All)
+            .await
+            .map_err(Error::ReadStreamError)?;
 
         let mut rv: Vec<AggregateEventEnvelope<A>> = vec![];
-        let result = self
-            .client
-            .read_stream(stream, &Default::default(), All)
-            .await;
 
-        if let Ok(mut stream) = result {
-            while let Some(event) = stream.next().await? {
-                let event_data = event.get_original_event();
+        while let Some(event) = result.next().await? {
+            let event_data = event.get_original_event();
 
-                // TODO: - can we try event eventlope ids as uuid in addition to usize?
-                // let uuid = event_data.id.clone();
-                let sequence = usize::try_from(event_data.revision).unwrap();
-                let event_payload = event_data
-                    .as_json::<ESDBEventPayload>()
-                    .map_err(Error::DeserializeEvent)?
-                    .event_envelope::<A>(sequence)?;
+            // TODO: - can we try event eventlope ids as uuid in addition to usize?
+            // let uuid = event_data.id.clone();
+            let event_payload = event_data
+                .as_json::<ESDBEventPayload>()
+                .map_err(Error::DeserializeEvent)?
+                .event_envelope::<A>(event_data.revision as usize)?;
 
-                rv.push(event_payload);
-            }
+            rv.push(event_payload);
         }
 
         Ok(rv)
@@ -105,29 +114,28 @@ impl EventStore for ESDBEventStore {
         A: Aggregate,
         <A as Aggregate>::Event: DeserializeOwned,
     {
-        let stream = <A as TypeId>::type_id().to_owned();
+
+        let mut result = self
+            .client
+            .read_stream(self.stream_id::<A>(None), &Default::default(), All)
+            .await
+            .map_err(Error::ReadStreamError)?;
+
         let mut rv: Vec<AggregateEventEnvelope<A>> = vec![];
 
-        let result = self
-            .client
-            .read_stream(stream, &Default::default(), All)
-            .await;
+        while let Some(event) = result.next().await? {
+            let event_data = event.get_original_event();
 
-        if let Ok(mut stream) = result {
-            while let Some(event) = stream.next().await? {
-                let event_data = event.get_original_event();
+            // TODO: - can we try event eventlope ids as uuid in addition to usize?
+            // let uuid = event_data.id.clone();
+            let sequence = usize::try_from(event_data.revision).unwrap();
+            if ids.contains(&sequence) {
+                let event_payload = event_data
+                    .as_json::<ESDBEventPayload>()
+                    .map_err(Error::DeserializeEvent)?
+                    .event_envelope::<A>(sequence)?;
 
-                // TODO: - can we try event eventlope ids as uuid in addition to usize?
-                // let uuid = event_data.id.clone();
-                let sequence = usize::try_from(event_data.revision).unwrap();
-                if ids.contains(&sequence) {
-                    let event_payload = event_data
-                        .as_json::<ESDBEventPayload>()
-                        .map_err(Error::DeserializeEvent)?
-                        .event_envelope::<A>(sequence)?;
-
-                    rv.push(event_payload);
-                }
+                rv.push(event_payload);
             }
         }
 
@@ -141,24 +149,19 @@ impl EventStore for ESDBEventStore {
     where
         A: Aggregate,
     {
-        let mut stream = <A as TypeId>::type_id().to_owned();
-        stream.push_str(&String::from(":"));
-        stream.push_str(&id.to_string());
-
         let options = ReadStreamOptions::default().position(StreamPosition::End);
-
-        let last_event = self
+        let result = self
             .client
-            .read_stream(&stream, &options, Single)
-            .map_err(Error::ReadStreamError)
-            .await?;
+            .read_stream(self.stream_id::<A>(Some(id)), &options, Single)
+            .await
+            .map_err(Error::ReadStreamError)?;
 
-        if let Ok(Some(event)) = last_event {
+        if let Ok(Some(event)) = result {
             let event_data = event.get_original_event();
-            Ok(Some(usize::try_from(event_data.revision).unwrap()))
-        } else {
-            Ok(None)
+            return Ok(Some(event_data.revision as usize))
         }
+
+        Ok(None)
     }
 
     async fn save_events<A>(
@@ -174,34 +177,15 @@ impl EventStore for ESDBEventStore {
             return Ok(vec![]);
         }
 
-        let mut stream = <A as TypeId>::type_id().to_owned();
-        stream.push_str(&String::from(":"));
-        stream.push_str(&id.to_string());
-
-        let sequence = self.load_aggregate_sequence::<A>(id).await?;
-        let mut event_ids = Vec::with_capacity(events.len());
-
-        let sequence = sequence.unwrap_or(0);
-        let revision = u64::try_from(sequence).unwrap();
-        let options =
-            AppendToStreamOptions::default().expected_revision(ExpectedRevision::Exact(revision));
-
+        let revision = self.load_aggregate_sequence::<A>(id).await?.unwrap_or(0);
         let mut payload: Vec<EventData> = vec![];
 
-        for (index, event) in events.iter().enumerate() {
-            let created_at = Utc::now();
-            let aggregate_type = <A as TypeId>::type_id().to_string();
-            let aggregate_id = id.to_string();
-            let sequence = sequence.clone() + index;
-            let id = sequence.clone();
-            let data = serde_json::to_value(event).map_err(Error::SerializeEvent)?;
-            event_ids.push(id.clone());
-
+        for event in events.iter() {
             let event_data_payload = ESDBEventPayload {
-                created_at,
-                aggregate_type,
-                aggregate_id,
-                event_data: data,
+                created_at: Utc::now(),
+                aggregate_type: <A as TypeId>::type_id().to_string(),
+                aggregate_id: id.to_string(),
+                event_data: serde_json::to_value(event).map_err(Error::SerializeEvent)?,
             };
 
             let event_data = EventData::json(event.event_type(), &event_data_payload)
@@ -211,12 +195,14 @@ impl EventStore for ESDBEventStore {
             payload.push(event_data);
         }
 
+        let options =
+            AppendToStreamOptions::default().expected_revision(ExpectedRevision::Exact(revision as u64));
         let _ = self
             .client
-            .append_to_stream(&stream, &options, payload)
-            .map_err(|err| Error::WriteStreamError(sequence, err))
+            .append_to_stream(&self.stream_id::<A>(Some(id)), &options, payload)
+            .map_err(|err| Error::WriteStreamError(revision as usize, err))
             .await?;
 
-        Ok(event_ids)
+        Ok((revision..events.len() - 1).collect())
     }
 }
