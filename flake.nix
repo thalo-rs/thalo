f01e3f6bdf17fdf2964f576555fdfffe59b3b1a8
Ari Seyhun <ariseyhun@live.com.au>
2023-11-22 13:50:26 +1030

    chore: add nix flake with direnv

--- flake.nix
+++ flake.nix
@@ -1,56 +1,33 @@
 {
-  description = "Example Rust development environment for Zero to Nix";
+  description = "Dev shell";
 
-  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
-    rust-overlay.url = "github:oxalica/rust-overlay"; # A helper for Rust + Nix
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  # Flake outputs
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      # Overlays enable you to customize the Nixpkgs attribute set
-      overlays = [
-        # Makes a `rust-bin` attribute available in Nixpkgs
-        (import rust-overlay)
-        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
-        # create a Rust environment
-        (self: super: {
-          rustToolchain = super.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-            extensions = [ "rust-src" ];
-            targets = [ "wasm32-wasi" "wasm32-unknown-unknown" ];
-          });
-        })
-      ];
-
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = (with pkgs; [
-            # The package provided by our custom overlay. Includes cargo, Clippy, cargo-fmt,
-            # rustdoc, rustfmt, and other tools.
-            rustToolchain
-            pkg-config
-            openssl
-            cmake
-          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-      });
-    };
+        rust-bin-nightly = (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)).override {
+          extensions = [ "rust-src" "rustfmt" "rust-analyzer" ];
+          targets = [ "wasm32-wasi" ];
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            pkg-config
+            protobuf
+            rust-bin-nightly
+          ];
+        };
+      }
+    );
 }
